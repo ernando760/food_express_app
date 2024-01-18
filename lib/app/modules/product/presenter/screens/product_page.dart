b1448d90830b5565957038ffeb8d40cf1800c304
ErnandoM <ernandomanoel.freire@gmail.com>
2024-01-18 14:58:29 -0300

    change: chamando um metodo onInit da classe ProductStateBuilder

--- lib/app/modules/product/presenter/screens/product_page.dart
+++ lib/app/modules/product/presenter/screens/product_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:food_express_app/app/modules/product/presenter/controllers/product_controller.dart';
+import 'package:food_express_app/app/modules/product/presenter/view_model/product_view_model.dart';
 import 'package:food_express_app/app/modules/product/presenter/widgets/custom_app_bar.dart';
 import 'package:food_express_app/app/modules/product/presenter/widgets/failure_product_widget.dart';
 import 'package:food_express_app/app/modules/product/presenter/widgets/loading_widget.dart';
-import 'package:food_express_app/app/modules/product/presenter/widgets/get_all_products_builder.dart';
+import 'package:food_express_app/app/modules/product/presenter/widgets/product_state_builder.dart';
 import 'package:food_express_app/app/modules/product/presenter/widgets/products_list_widget.dart';
 import 'package:food_express_app/app/modules/shared/constant/colors.dart';
 
@@ -13,11 +13,11 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final controller = context.watch<ProductController>();
+    final controller = context.watch<ProductViewModel>((bind) => bind.state);
     return Scaffold(
       appBar: const CustomAppBar(title: "Food Express"),
-      body: GetAllProductsBuilder(
-        controller: controller,
+      body: ProductStateBuilder(
+        onInit: () => Future(() async => controller.getAllProducts()),
         loading: () => const LoadingWidget(),
         loaded: (products) => ProductsListWidget(products: products ?? []),
         failure: (messageError, isConnectionError) => FailureProductWidget(
